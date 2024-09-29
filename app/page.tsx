c99ad1d2cbe8a90dbf1fa9a4893f32a8167758cc
11vinicius <viniciuscesarlemes@gmail.com>
2024-09-29 11:45:08 -0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { Post } from './posts.model';
 import api from './shared/baseService';
-import Card from './components/123';
+import Card from './components/Card';
 
 export default function Home() {
 
